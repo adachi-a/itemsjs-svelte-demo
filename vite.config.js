ca0cb873a6116301e3e1fbc0c158b50da966cec9
A.Adachi <17152337+adachi-a@users.noreply.github.com>
2023-10-13 14:54:41 +0900

    edit config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   base: '/itemsjs-svelte-demo/',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        koban: 'index.html',
+        animation: 'animation.html'
+      }
+    }
+  }
+});
